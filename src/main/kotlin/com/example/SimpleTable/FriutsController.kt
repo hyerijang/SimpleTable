64da269e769af7d50bb4d5ecad9195c75954dd8a
Hyeri Jang <chjang5621@naver.com>
2024-06-03 23:42:28 +0900

    feat : 간단한 select 예제를 위한 컨트롤러 구현

--- src/main/kotlin/com/example/SimpleTable/FriutsController.kt
+++ src/main/kotlin/com/example/SimpleTable/FriutsController.kt
@@ -1,14 +1,37 @@
 package com.example.SimpleTable.table
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/v1/fruits")
-class FriutsController {
+class FruitsController {
+
+    private val logger: Logger = LoggerFactory.getLogger(FruitsController::class.java)
 
     @GetMapping
-    fun getName(@RequestParam(required = false) name: String?): String {
-        return name ?: "ALL"
+    fun getAll(@RequestParam(required = false) name: String?): List<Fruit> {
+        val fruits = listOf(
+            Fruit(1, "apple" , 123),
+            Fruit(2, "banana", 111)
+        )
+        return if (name.isNullOrEmpty()) {
+            fruits
+        } else {
+            fruits.filter { it.name.contains(name, ignoreCase = true) }
+        }
     }
 
+    @GetMapping("/{name}")
+    fun getName(@PathVariable name: String): Fruit {
+        logger.info("선택된 과일의 이름 : {}", name)
+        return Fruit((Math.random() * 1000).toLong(), name, 1000)
+    }
 }
+
+data class Fruit(
+    val id: Long,
+    val name: String,
+    val price: Long?
+)
